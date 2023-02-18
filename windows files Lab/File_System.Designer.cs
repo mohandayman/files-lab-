e7db1c0b3db85220dc8d8e4879e82463f57a0da2
mohandayman <66260018+mohandayman@users.noreply.github.com>
2023-02-19 01:55:16 +0200

    files lab

---
+++ windows files Lab/File_System.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class File_System
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            ""});
+            this.listBox1.Location = new System.Drawing.Point(45, 64);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(193, 244);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(509, 64);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(193, 244);
+            this.listBox2.TabIndex = 2;
+            this.listBox2.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox2_MouseDoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(363, 127);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = ">>";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(363, 214);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "<<";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(191, 353);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Copy";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(329, 353);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Delete";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(476, 353);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Back";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(54, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(175, 22);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(519, 36);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(175, 22);
+            this.textBox2.TabIndex = 10;
+            // 
+            // File_System
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "File_System";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ windows files Lab/File_System.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.ListBox;
+
+namespace WindowsFormsApp1
+{
+    public partial class File_System : Form
+    {
+        DriveInfo[] All_Drives;
+
+        /*      var current_Dirctory = Directory.SetCurrentDirectory("");*/
+
+
+
+        public File_System()
+        {
+            InitializeComponent();
+            All_Drives = DriveInfo.GetDrives();
+            listBox1.Items.AddRange(All_Drives);
+            listBox2.Items.AddRange(All_Drives);
+
+
+
+
+
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (listBox1.SelectedItem.ToString() != textBox1.Text)      // check of click The Same Path
+
+                textBox1.Text = listBox1.SelectedItem.ToString();   // change Text box To path 
+
+            var selected_item = listBox1.SelectedItem;
+
+            DirectoryInfo parent_selected = Directory.GetParent(textBox1.Text);  //  parent 
+
+            string root_selected_path = Directory.GetDirectoryRoot(textBox1.Text);  // root path 
+
+            var root_selected = new DirectoryInfo(root_selected_path); //  root obj 
+
+
+            listBox1.Items.Clear();
+
+            listBox1.Items.Add(selected_item);
+
+            if (parent_selected != null)  // check of The root Directory
+            {
+                listBox1.Items.Add(parent_selected);
+
+                listBox1.Items.Add(root_selected);
+            }
+
+
+            if (selected_item is DirectoryInfo || selected_item is Directory || selected_item is DriveInfo)
+            {
+                string[] sub_directories_Paths = Directory.GetDirectories(textBox1.Text);
+
+                foreach (string subdir in sub_directories_Paths)
+                {
+
+                    DirectoryInfo sub_Directory = new DirectoryInfo(subdir);
+
+                    listBox1.Items.Add(sub_Directory);
+
+
+                }
+
+                string[] sub_Files_Paths = Directory.GetFiles(textBox1.Text);
+                foreach (string subFile in sub_Files_Paths)
+                {
+                    listBox1.Items.Add(new FileInfo(subFile));
+                }
+
+
+            }
+            else
+                Process.Start("notepad.exe", textBox1.Text);
+
+
+
+
+
+
+
+
+
+
+        }
+
+        private void listBox2_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (listBox2.SelectedItem.ToString() != textBox2.Text)      // check of click The Same Path
+
+                textBox2.Text = listBox2.SelectedItem.ToString();   // change Text box To path 
+
+            var selected_item = listBox2.SelectedItem;
+
+            DirectoryInfo parent_selected = Directory.GetParent(textBox2.Text);  //  parent 
+
+            string root_selected_path = Directory.GetDirectoryRoot(textBox2.Text);  // root path 
+
+            var root_selected = new DirectoryInfo(root_selected_path); //  root obj 
+
+
+            listBox2.Items.Clear();
+
+            listBox2.Items.Add(selected_item);
+
+            if (parent_selected != null)  // check of The root Directory
+            {
+                listBox2.Items.Add(parent_selected);
+
+                listBox2.Items.Add(root_selected);
+            }
+
+            if (selected_item is DirectoryInfo || selected_item is Directory || selected_item is DriveInfo)
+            {
+
+                string[] sub_directories_Paths = Directory.GetDirectories(textBox2.Text);
+
+                foreach (string subdir in sub_directories_Paths)
+                {
+
+                    DirectoryInfo sub_Directory = new DirectoryInfo(subdir);
+
+                    listBox2.Items.Add(sub_Directory);
+
+
+                }
+
+                string[] sub_Files_Paths = Directory.GetFiles(textBox2.Text);
+                foreach (string subFile in sub_Files_Paths)
+                {
+                    listBox2.Items.Add(new FileInfo(subFile));
+                }
+
+
+            }
+            else
+                Process.Start("notepad.exe", textBox2.Text);
+
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string source_path = $"{textBox1.Text}";
+            string Destnation_path = $"{textBox2.Text}";
+
+            if (Directory.Exists(Destnation_path))
+            {
+                DirectoryInfo d1 = new DirectoryInfo(source_path);
+                string Dest_folder_name = d1.Name;
+                Directory.Move(source_path, $@"{Destnation_path}\{Dest_folder_name}");
+
+            }
+            else
+            {
+                Directory.Move(source_path, Destnation_path);
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string source_path = $"{textBox2.Text}";
+            string Destnation_path = $"{textBox1.Text}";
+
+            if (Directory.Exists(Destnation_path))
+            {
+                DirectoryInfo d1 = new DirectoryInfo(source_path);
+                string Dest_folder_name = d1.Name;
+                Directory.Move(source_path, $@"{Destnation_path}\{Dest_folder_name}");
+                //Directory.Delete(source_path);
+            }
+            else
+            {
+                Directory.Move(source_path, Destnation_path);
+
+            }
+
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DateTime List1_LastAccess;
+            DateTime List2_LastAccess;
+
+            if (Directory.Exists(textBox1.Text))
+            {
+                List1_LastAccess = Directory.GetLastAccessTime(textBox1.Text);
+            }
+            else
+            {
+                List1_LastAccess = File.GetLastAccessTime(textBox1.Text);
+            }
+
+
+            if (Directory.Exists(textBox2.Text))
+            {
+
+                List2_LastAccess = Directory.GetLastAccessTime(textBox2.Text);
+            }
+            else
+            {
+                List2_LastAccess = File.GetLastAccessTime(textBox2.Text);
+            }
+
+
+            var result = DateTime.Compare(List1_LastAccess, List2_LastAccess);
+
+            if (result > 0)
+            {
+                if (Directory.Exists(textBox1.Text))
+                    Directory.Delete(textBox1.Text, true);
+                else
+                    File.Delete(textBox1.Text);
+            }
+            else
+            {
+                if (Directory.Exists(textBox2.Text))
+                {
+                    Directory.Delete(textBox2.Text, true);
+                }
+                else
+                {
+                    File.Delete(textBox2.Text);
+                }
+
+            }
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            DirectoryInfo parent_selected1;
+            DirectoryInfo parent_selected2;
+
+            DateTime List1_LastAccess;
+            DateTime List2_LastAccess;
+            if (Directory.Exists(textBox1.Text))
+            {
+                List1_LastAccess = Directory.GetLastAccessTime(textBox1.Text);
+            }
+            else
+            {
+                List1_LastAccess = File.GetLastAccessTime(textBox1.Text);
+            }
+
+
+            if (Directory.Exists(textBox2.Text))
+            {
+
+                List2_LastAccess = Directory.GetLastAccessTime(textBox2.Text);
+            }
+            else
+            {
+                List2_LastAccess = File.GetLastAccessTime(textBox2.Text);
+            }
+
+
+            var result = DateTime.Compare(List1_LastAccess, List2_LastAccess);
+
+            if (result > 0)
+            {
+
+                parent_selected1 = Directory.GetParent(textBox1.Text);  //  parent 
+                change_directory(parent_selected1,listBox1);
+
+            }
+            else
+            {
+                parent_selected2 = Directory.GetParent(textBox2.Text);  //  parent 
+                change_directory(parent_selected2, listBox2);
+
+
+
+
+
+            }
+        }
+
+
+
+
+        public void change_directory(DirectoryInfo parent , ListBox listbox)
+        {
+            if (parent != null) { 
+            textBox1.Text = parent.Name;
+            
+
+            DirectoryInfo parent_selected = Directory.GetParent(parent.Name);  //  parent 
+
+            string root_selected_path = Directory.GetDirectoryRoot(parent.Name);  // root path 
+
+            var root_selected = new DirectoryInfo(root_selected_path); //  root obj 
+
+
+            listbox.Items.Clear();
+
+            listbox.Items.Add(parent);
+
+            if (parent_selected != null)  // check of The root Directory
+            {
+                listbox.Items.Add(parent_selected);
+
+                listbox.Items.Add(root_selected);
+            }
+
+
+                if (Directory.Exists(parent.Name))
+                {
+                    string[] sub_directories_Paths = Directory.GetDirectories(parent.Name);
+
+                    foreach (string subdir in sub_directories_Paths)
+                    {
+
+                        DirectoryInfo sub_Directory = new DirectoryInfo(subdir);
+
+                        listbox.Items.Add(sub_Directory);
+
+
+                    }
+
+                    string[] sub_Files_Paths = Directory.GetFiles(textBox1.Text);
+                    foreach (string subFile in sub_Files_Paths)
+                    {
+                        listbox.Items.Add(new FileInfo(subFile));
+                    }
+
+                }
+            }
+           
+
+
+
+        }
+    }
+}
